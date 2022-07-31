9178b65aac1d41b5d321e041e86b158172279c92
Dmitrii Derepko <xepozz@list.ru>
2022-07-31 20:06:00 +0300

    Move out backend url to envs

--- src/API/Debug.ts
+++ src/API/Debug.ts
@@ -4,10 +4,9 @@ interface Pokemon {
     [name: string]: string | object | any
 }
 
-// Define a service using a base URL and expected endpoints
 export const debugApi = createApi({
     reducerPath: 'api.debug',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://127.0.0.1:8080/debug/api/'}),
+    baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_BACKEND_URL+ '/debug/api/'}),
     endpoints: (builder) => ({
         getDebug: builder.query<Pokemon, string>({
             query: () => ``,
@@ -15,6 +14,4 @@ export const debugApi = createApi({
     }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const {useGetDebugQuery} = debugApi
--- src/API/Inspector/Inspector.ts
+++ src/API/Inspector/Inspector.ts
@@ -4,10 +4,9 @@ interface Pokemon {
     [name: string]: string | object | []
 }
 
-// Define a service using a base URL and expected endpoints
 export const inspectorApi = createApi({
     reducerPath: 'api.inspector',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://127.0.0.1:8080/inspect/api/'}),
+    baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_BACKEND_URL + '/inspect/api/'}),
     endpoints: (builder) => ({
         getParameters: builder.query<Pokemon, string>({
             query: () => `params`,
@@ -24,6 +23,4 @@ export const inspectorApi = createApi({
     }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const {useGetParametersQuery, useGetConfigurationQuery, useGetClassesQuery, useLazyGetObjectQuery} = inspectorApi
