200017bb3425586f8bf1a3d290c2d9eca50c9f50
Dmitrii Derepko <xepozz@list.ru>
2022-11-27 22:30:06 +0300

    Add ability to edit json

--- src/Component/JsonRenderer.tsx
+++ src/Component/JsonRenderer.tsx
@@ -1,4 +1,4 @@
-import {DataType, JsonViewer} from '@textea/json-viewer';
+import {DataType, JsonViewer, JsonViewerOnChange} from '@textea/json-viewer';
 import * as React from 'react';
 
 const REGEXP_PHP_FUNCTION = /(static )?(function |fn )\(.*\).*((\{.*})|(=>.*))/s;
@@ -6,9 +6,17 @@ const REGEXP_PHP_FUNCTION = /(static )?(function |fn )\(.*\).*((\{.*})|(=>.*))/s
 export type JsonRendererProps = {
     value: any;
     depth?: number;
+    editable?: boolean;
+    onChange?: JsonViewerOnChange;
     valueTypes?: DataType<any>[];
 };
-export const JsonRenderer = ({value, depth = 5, valueTypes = []}: JsonRendererProps) => {
+export const JsonRenderer = ({
+    value,
+    depth = 5,
+    editable = false,
+    onChange = undefined,
+    valueTypes = [],
+}: JsonRendererProps) => {
     if (typeof value == 'string' && value.match(REGEXP_PHP_FUNCTION)?.length) {
         const html = value.replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp');
         return <div dangerouslySetInnerHTML={{__html: html}} />;
@@ -18,6 +26,8 @@ export const JsonRenderer = ({value, depth = 5, valueTypes = []}: JsonRendererPr
         <JsonViewer
             rootName={false}
             value={value}
+            editable={editable}
+            onChange={onChange}
             enableClipboard={true}
             defaultInspectDepth={depth}
             groupArraysAfterLength={50}
