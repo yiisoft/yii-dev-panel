ea8efff207b82ea8c8e0cd2a9021094dbea60b86
Dmitrii Derepko <xepozz@list.ru>
2023-05-01 23:53:34 +0300

    Save example

--- packages/yii-dev-panel/src/App.tsx
+++ packages/yii-dev-panel/src/App.tsx
@@ -21,3 +21,37 @@ export default function App() {
         </Provider>
     );
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
+const sse = () => {
+    // const button = document.querySelector('button');
+    const evtSource = new EventSource('http://127.0.0.1:8080/debug/api/sse');
+    // const evtSource = new EventSource('http://localhost:8080/see.php', {
+    //   withCredentials: true,
+    // });
+    console.log(evtSource.withCredentials);
+    console.log(evtSource.readyState);
+    console.log(evtSource.url);
+
+    evtSource.onopen = function () {
+        console.log('Connection to server opened.');
+    };
+
+    evtSource.onmessage = function (e) {
+        console.log('message: ', e);
+    };
+
+    evtSource.onerror = function () {
+        console.log('EventSource failed.');
+    };
+
+    // button.onclick = function () {
+    //     console.log('Connection closed');
+    //     evtSource.close();
+    // };
+};
+try {
+    // sse();
+} catch (e) {
+    console.log(e);
+}
