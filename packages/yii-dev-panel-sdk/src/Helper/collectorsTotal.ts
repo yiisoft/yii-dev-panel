3422e7fef34072ae87d81bd2599dd146e9ded0a7
Dmitrii Derepko <xepozz@list.ru>
2023-06-11 17:42:00 +0300

    Add check for null properties

--- packages/yii-dev-panel-sdk/src/Helper/collectorsTotal.ts
+++ packages/yii-dev-panel-sdk/src/Helper/collectorsTotal.ts
@@ -4,29 +4,33 @@ import {CollectorsMap} from '@yiisoft/yii-dev-panel-sdk/Helper/collectors';
 export const getCollectedCountByCollector = (collector: CollectorsMap, data: DebugEntry): number => {
     switch (collector) {
         case CollectorsMap.AssetCollector:
-            return data.asset.bundles.total;
+            return Number(data.asset?.bundles?.total);
         case CollectorsMap.DatabaseCollector:
-            return data.db.queries.total + data.db.transactions.total;
+            return Number(data.db?.queries?.total) + Number(data.db?.transactions?.total);
         case CollectorsMap.ExceptionCollector:
-            return Object.values(data.exception).length > 0 ? 1 : 0;
+            return Object.values(data.exception ?? []).length > 0 ? 1 : 0;
         case CollectorsMap.EventCollector:
-            return data.event.total;
+            return Number(data.event?.total);
         case CollectorsMap.LogCollector:
-            return data.logger.total;
+            return Number(data.logger?.total);
         case CollectorsMap.ServiceCollector:
-            return data.service.total;
+            return Number(data.service?.total);
         case CollectorsMap.ValidatorCollector:
-            return data.validator.total + data.validator.invalid + data.validator.valid;
+            return Number(data.validator?.total) + Number(data.validator?.invalid) + Number(data.validator?.valid);
         case CollectorsMap.MiddlewareCollector:
-            return data.middleware.total;
+            return Number(data.middleware?.total);
         case CollectorsMap.QueueCollector:
-            return data.queue.countPushes + data.queue.countStatuses + data.queue.countProcessingMessages;
+            return (
+                Number(data.queue?.countPushes) +
+                Number(data.queue?.countStatuses) +
+                Number(data.queue?.countProcessingMessages)
+            );
         case CollectorsMap.HttpClientCollector:
-            return data.http.count;
+            return Number(data.http?.count);
         case CollectorsMap.HttpStreamCollector:
-            return data.http_stream.length;
+            return Number(data.http_stream?.length);
         case CollectorsMap.FilesystemStreamCollector:
-            return Object.values(data.fs_stream).reduce((acc, value) => acc + value, 0);
+            return Object.values(data.fs_stream ?? []).reduce((acc, value) => acc + value, 0);
         case CollectorsMap.ConsoleAppInfoCollector:
             return 0;
         default:
