517e32ef16d80717eb1e92cc0a4b4ce6fd43294a
Dmitrii Derepko <xepozz@list.ru>
2023-06-27 23:26:28 +0300

    Add sw plugin, fix federation plugin

--- packages/yii-dev-panel/vite.config.ts
+++ packages/yii-dev-panel/vite.config.ts
@@ -1,45 +1,73 @@
 import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-const {dependencies} = require('../yii-dev-panel-sdk/package.json');
-
-const sharedModules = Object.entries(dependencies).map(([name, version]) => name);
-export default defineConfig(async ({command}) => ({
-    server: {
-        open: false,
-        port: 3000,
-        fs: {
-            allow: ['.'],
+const sharedModules = ['react', 'react-dom', 'react-redux', 'react-router', 'react-router-dom', 'redux-persist'];
+export default defineConfig(async ({command}) => {
+    return {
+        server: {
+            open: false,
+            port: 3000,
+            fs: {
+                allow: ['.'],
+            },
         },
-    },
-    resolve: {
-        alias: {
-            // Needed for `useSelector` tracking in wdyr.tsx: https://github.com/welldone-software/why-did-you-render/issues/85
-            'react-redux': 'react-redux/dist/react-redux.js',
-            '@yiisoft/yii-dev-panel/*': '../yii-dev-panel/src/*',
-            '@yiisoft/yii-dev-panel-sdk/*': '../yii-dev-panel-sdk/src/*',
-            '@yiisoft/yii-dev-toolbar/*': '../yii-dev-toolbar/src/*',
+        resolve: {
+            alias: {
+                // Needed for `useSelector` tracking in wdyr.tsx: https://github.com/welldone-software/why-did-you-render/issues/85
+                'react-redux': 'react-redux/dist/react-redux.js',
+                '@yiisoft/yii-dev-panel/*': '../yii-dev-panel/src/*',
+                '@yiisoft/yii-dev-panel-sdk/*': '../yii-dev-panel-sdk/src/*',
+                '@yiisoft/yii-dev-toolbar/*': '../yii-dev-toolbar/src/*',
+            },
         },
-    },
-    plugins: [
-        react({
-            jsxImportSource: '@welldone-software/why-did-you-render',
-        }),
-        viteTsconfigPaths(),
-        svgrPlugin(),
-        federation({
-            name: 'host',
-            remotes: {},
-            shared: sharedModules,
-        }),
-    ],
-    base: './',
-    build: {
-        minify: 'terser',
-        outDir: 'dist',
-        target: 'esnext',
-    },
-}));
+        plugins: [
+            react({
+                jsxImportSource: '@welldone-software/why-did-you-render',
+            }),
+            VitePWA({
+                injectRegister: 'script',
+                strategies: 'injectManifest',
+                srcDir: 'src',
+                filename: 'service-worker.js',
+                devOptions: {
+                    enabled: process.env.NODE_ENV === 'development',
+                    type: 'module',
+                },
+            }),
+            viteTsconfigPaths(),
+            svgrPlugin(),
+            federation({
+                name: 'host',
+                remotes: {},
+                shared: sharedModules,
+            }),
+        ],
+        base: process.env.VITE_ENV === 'github' ? '/yii-dev-panel/' : '/',
+        build: {
+            minify: 'terser',
+            outDir: 'dist',
+            target: 'esnext',
+            // rollupOptions: {
+            //     input: {
+            //         // the default entry point
+            //         app: './index.html',
+            //
+            //         // 1️⃣
+            //         'service-worker': './src/service-worker.js',
+            //     },
+            //     output: {
+            //         // 2️⃣
+            //         entryFileNames: (assetInfo) => {
+            //             return assetInfo.name === 'service-worker'
+            //                 ? '[name].js' // put service worker in root
+            //                 : 'assets/js/[name]-[hash].js'; // others in `assets/js/`
+            //         },
+            //     },
+            // },
+        },
+    };
+});
