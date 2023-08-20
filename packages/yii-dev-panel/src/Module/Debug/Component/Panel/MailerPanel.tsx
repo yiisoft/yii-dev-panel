2998f56ab52688bbe8303ea631a50f6704288bc4
Dmitrii Derepko <xepozz@list.ru>
2023-08-20 21:21:08 +0300

    Improve mailer panel

--- packages/yii-dev-panel/src/Module/Debug/Component/Panel/MailerPanel.tsx
+++ packages/yii-dev-panel/src/Module/Debug/Component/Panel/MailerPanel.tsx
@@ -1,6 +1,53 @@
-import {List, ListItemButton, ListItemSecondaryAction, ListItemText} from '@mui/material';
+import HtmlIcon from '@mui/icons-material/Html';
+import RawOnIcon from '@mui/icons-material/RawOn';
+import {Button, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText} from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Typography from '@mui/material/Typography';
+import {nl2br} from '@yiisoft/yii-dev-panel-sdk/Helper/nl2br';
+import {useCallback, useState} from 'react';
 
+type PreviewType = 'html' | 'raw';
+type PreviewDialogProps = {
+    onClose: () => void;
+    open: boolean;
+    previewType: PreviewType;
+    message: MailMessageType;
+};
+const PreviewDialog = ({message, open, onClose, previewType}: PreviewDialogProps) => {
+    if (!message) {
+        return null;
+    }
+    console.log('message', message);
+    return (
+        <Dialog
+            open={open}
+            onClose={onClose}
+            fullScreen
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle>{message.subject}</DialogTitle>
+            <DialogContent>
+                {previewType === 'html' ? (
+                    <DialogContentText dangerouslySetInnerHTML={{__html: message.htmlBody}}></DialogContentText>
+                ) : (
+                    <DialogContentText>{nl2br(message.raw)}</DialogContentText>
+                )}
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose} autoFocus>
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
 type MailMessageType = {
     from: Record<string, string>;
     to: Record<string, string>;
@@ -8,6 +55,7 @@ type MailMessageType = {
     date: string;
     textBody: string;
     htmlBody: string;
+    raw: string;
     charset: string;
     replyTo: Record<string, string>;
     cc: Record<string, string>;
@@ -26,6 +74,18 @@ function serializeSender(sender: Record<string, string>): string {
 }
 
 export const MailerPanel = ({data}: MailerPanelProps) => {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [previewType, selectPreviewType] = useState<PreviewType>('html');
+    const [selectedMessage, setSelectedMessage] = useState<MailMessageType | null>(null);
+    const handleDialogClose = useCallback(() => {
+        setDialogOpen(false);
+        setSelectedMessage(null);
+    }, []);
+    const selectMessage = (message: MailMessageType) => {
+        setSelectedMessage(message);
+        setDialogOpen(true);
+    };
+
     return (
         <>
             {!data || data.messages.length === 0 ? (
@@ -33,20 +93,47 @@ export const MailerPanel = ({data}: MailerPanelProps) => {
             ) : (
                 <List>
                     {data.messages.map((entry, index) => (
-                        <ListItemButton>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>{index + 1}</Avatar>
+                            </ListItemAvatar>
                             <ListItemText
                                 primary={entry.subject}
                                 secondary={`From: ${serializeSender(entry.to)}, to: ${serializeSender(entry.to)}`}
                             />
                             <ListItemSecondaryAction>
+                                <IconButton
+                                    edge="end"
+                                    onClick={() => {
+                                        selectPreviewType('html');
+                                        selectMessage(entry);
+                                    }}
+                                >
+                                    <HtmlIcon />
+                                </IconButton>
+                                <IconButton
+                                    edge="end"
+                                    onClick={() => {
+                                        selectPreviewType('raw');
+                                        selectMessage(entry);
+                                    }}
+                                >
+                                    <RawOnIcon />
+                                </IconButton>
                                 <Typography variant="body2" color="textSecondary">
                                     {entry.date}
                                 </Typography>
                             </ListItemSecondaryAction>
-                        </ListItemButton>
+                        </ListItem>
                     ))}
                 </List>
             )}
+            <PreviewDialog
+                open={dialogOpen}
+                onClose={handleDialogClose}
+                message={selectedMessage}
+                previewType={previewType}
+            />
         </>
     );
 };
