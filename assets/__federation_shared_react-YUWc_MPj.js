b0f15036e56f9c40e8c103b77a4957b1df95d0e9
Dmitrii Derepko <xepozz@list.ru>
2024-04-07 18:29:59 +0700

    Updates

---
+++ assets/__federation_shared_react-YUWc_MPj.js
@@ -0,0 +1,1 @@
+import{r as s,g as a}from"./index-Cxp9oPON.js";const o=a(s());export{o as default};
---
+++ assets/__federation_shared_react-dom--yO6rQ29.js
@@ -0,0 +1,1 @@
+import{g as o}from"./index-Cxp9oPON.js";import{r}from"./index-Cu8_3Doc.js";const s=o(r());export{s as default};
